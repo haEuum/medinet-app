e0c4f4f98b6ce834260a61a5fadbf2d696af20e3
wodnr16 <bagjaeug29@gmail.com>
2025-03-17 20:05:20 +0900

    feature :: setting

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,27 @@
-module.exports = {
-    presets: ['module:@react-native/babel-preset'],
-    plugins: [
-        [
-            "module:react-native-dotenv",
-            {
-                moduleName: "@env",
-                path: ".env",
-                allowUndefined: false,
-            }
-        ]
-    ]
+module.exports = function (api) {
+    api.cache(true);
+
+    return {
+        presets: ['@react-native/babel-preset'],
+        plugins: [
+            [
+                "module:react-native-dotenv",
+                {
+                    moduleName: "@env",
+                    path: ".env",
+                    allowUndefined: false,
+                },
+            ],
+            "@babel/plugin-transform-runtime",
+            [
+                "module-resolver",
+                {
+                    alias: {
+                        "src": "./src"
+                    }
+                }
+            ],
+            process.env.NODE_ENV === "production" ? "transform-remove-console" : null,
+        ].filter(Boolean)
+    };
 };
