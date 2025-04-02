49d9cb34ea1ea505092745e0fb72f06384ec9ff5
박상민 <a01071397193@gmail.com>
2025-04-02 17:20:25 +0900

    Update :: rem -> px

--- src/design/theme/typography.ts
+++ src/design/theme/typography.ts
@@ -1,132 +1,131 @@
-import { css } from 'styled-components';
+import { css } from 'styled-components/native';
 
 export const Display1Bold = css`
-  font-size: 2.25rem; // 36px
-  font-weight: 700;
-  font-family: 'Pretendard-Bold';
+    font-size: 36px;
+    font-weight: 700;
+    font-family: 'Pretendard-Bold';
 `;
 
 export const Display1Medium = css`
-  font-size: 2.25rem;
-  font-weight: 500;
-  font-family: 'Pretendard-Medium';
+    font-size: 36px;
+    font-weight: 500;
+    font-family: 'Pretendard-Medium';
 `;
 
 export const Display1Regular = css`
-  font-size: 2.25rem;
-  font-weight: 400;
-  font-family: 'Pretendard-Regular';
+    font-size: 36px;
+    font-weight: 400;
+    font-family: 'Pretendard-Regular';
 `;
 
 export const Display2Bold = css`
-  font-size: 2rem; // 32px
-  font-weight: 700;
-  font-family: 'Pretendard-Bold';
+    font-size: 32px;
+    font-weight: 700;
+    font-family: 'Pretendard-Bold';
 `;
 
 export const Display2Medium = css`
-  font-size: 2rem;
-  font-weight: 500;
-  font-family: 'Pretendard-Medium';
+    font-size: 32px;
+    font-weight: 500;
+    font-family: 'Pretendard-Medium';
 `;
 
 export const Display2Regular = css`
-  font-size: 2rem;
-  font-weight: 400;
-  font-family: 'Pretendard-Regular';
+    font-size: 32px;
+    font-weight: 400;
+    font-family: 'Pretendard-Regular';
 `;
 
 export const Title1Bold = css`
-  font-size: 1.75rem; // 28px
-  font-weight: 700;
-  font-family: 'Pretendard-Bold';
+    font-size: 28px;
+    font-weight: 700;
+    font-family: 'Pretendard-Bold';
 `;
 
 export const Title1Medium = css`
-  font-size: 1.75rem;
-  font-weight: 500;
-  font-family: 'Pretendard-Medium';
+    font-size: 28px;
+    font-weight: 500;
+    font-family: 'Pretendard-Medium';
 `;
 
 export const Title1Regular = css`
-  font-size: 1.75rem;
-  font-weight: 400;
-  font-family: 'Pretendard-Regular';
+    font-size: 28px;
+    font-weight: 400;
+    font-family: 'Pretendard-Regular';
 `;
 
 export const Title2Bold = css`
-  font-size: 1.5rem; // 24px
-  font-weight: 700;
-  font-family: 'Pretendard-Bold';
+    font-size: 24px;
+    font-weight: 700;
+    font-family: 'Pretendard-Bold';
 `;
 
 export const Title2Medium = css`
-  font-size: 1.5rem;
-  font-weight: 500;
-  font-family: 'Pretendard-Medium';
+    font-size: 24px;
+    font-weight: 500;
+    font-family: 'Pretendard-Medium';
 `;
 
 export const Title2Regular = css`
-  font-size: 1.5rem;
-  font-weight: 400;
-  font-family: 'Pretendard-Regular';
+    font-size: 24px;
+    font-weight: 400;
+    font-family: 'Pretendard-Regular';
 `;
 
 export const BodyBold = css`
-  font-size: 1rem; // 16px
-  font-weight: 700;
-  font-family: 'Pretendard-Bold';
+    font-size: 16px;
+    font-weight: 700;
+    font-family: 'Pretendard-Bold';
 `;
 
 export const BodyMedium = css`
-  font-size: 1rem;
-  font-weight: 500;
-  font-family: 'Pretendard-Medium';
+    font-size: 16px;
+    font-weight: 500;
+    font-family: 'Pretendard-Medium';
 `;
 
 export const BodyRegular = css`
-  font-size: 1rem;
-  font-weight: 400;
-  font-family: 'Pretendard-Regular';
+    font-size: 16px;
+    font-weight: 400;
+    font-family: 'Pretendard-Regular';
 `;
 
 export const LabelBold = css`
-  font-size: 0.875rem; // 14px
-  font-weight: 700;
-  font-family: 'Pretendard-Bold';
+    font-size: 14px;
+    font-weight: 700;
+    font-family: 'Pretendard-Bold';
 `;
 
 export const LabelMedium = css`
-  font-size: 0.875rem;
-  font-weight: 500;
-  font-family: 'Pretendard-Medium';
+    font-size: 14px;
+    font-weight: 500;
+    font-family: 'Pretendard-Medium';
 `;
 
 export const LabelRegular = css`
-  font-size: 0.875rem;
-  font-weight: 400;
-  font-family: 'Pretendard-Regular';
+    font-size: 14px;
+    font-weight: 400;
+    font-family: 'Pretendard-Regular';
 `;
 
 export const CaptionBold = css`
-  font-size: 0.75rem; // 12px
-  font-weight: 700;
-  font-family: 'Pretendard-Bold';
+    font-size: 12px;
+    font-weight: 700;
+    font-family: 'Pretendard-Bold';
 `;
 
 export const CaptionMedium = css`
-  font-size: 0.75rem;
-  font-weight: 500;
-  font-family: 'Pretendard-Medium';
+    font-size: 12px;
+    font-weight: 500;
+    font-family: 'Pretendard-Medium';
 `;
 
 export const CaptionRegular = css`
-  font-size: 0.75rem;
-  font-weight: 400;
-  font-family: 'Pretendard-Regular';
+    font-size: 12px;
+    font-weight: 400;
+    font-family: 'Pretendard-Regular';
 `;
 
-// ✅ 객체로 한 번에 export (필요에 따라 개별 import 가능)
 export const Typography = {
     Display1Bold,
     Display1Medium,
