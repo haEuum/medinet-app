c65283e4c2c4f544b905a3df842c455764fecd90
박상민 <a01071397193@gmail.com>
2025-04-26 23:53:29 +0900

    Update :: eject 병합 오류 수정

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,20 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.sourceExts.push('cjs');
+// svg 설정 추가
+const { assetExts, sourceExts } = config.resolver;
+
+config.resolver.assetExts = assetExts.filter(ext => ext !== 'svg');
+config.resolver.sourceExts = [...sourceExts, 'svg', 'cjs'];
 
 config.resolver.alias = {
     src: './src',
+};
+
+// transformer 설정 추가
+config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
 };
 
 module.exports = config;
