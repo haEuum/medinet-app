4546835789895fb69e9e96959fc569c6de36cf86
박상민 <a01071397193@gmail.com>
2025-07-28 20:30:32 +0900

    style :: 공백 수정

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import Home from "src/components/ui/icons/bottombar/home";
 import Profile from "src/components/ui/icons/bottombar/profile";
 import Chat from "src/components/ui/icons/bottombar/chat";
 import Triage from "src/components/ui/icons/bottombar/triage";
-import {Platform} from "react-native";
+import { Platform } from "react-native";
 
 export default function TabsLayout() {
     return (
@@ -13,12 +13,19 @@ export default function TabsLayout() {
                 tabBarActiveTintColor: "#0047AB",
                 tabBarInactiveTintColor: "#7F8C8D",
                 tabBarStyle: {
+                    position: "absolute",
                     backgroundColor: "#fff",
                     height: 60,
+                    marginBottom: Platform.OS === "ios" ? 24 : 12,
+                    marginHorizontal: 16,
                     paddingBottom: Platform.OS === "ios" ? 20 : 8,
                     paddingTop: 6,
-                    borderTopWidth: 0.5,
-                    borderTopColor: "#ccc",
+                    borderRadius: 20,
+                    elevation: 5,
+                    shadowColor: "#000",
+                    shadowOffset: { width: 0, height: 3 },
+                    shadowOpacity: 0.06,
+                    shadowRadius: 5,
                 },
             }}
         >
@@ -33,21 +40,21 @@ export default function TabsLayout() {
                 name="chat/index"
                 options={{
                     tabBarIcon: ({ color }) => <Chat fill={color} />,
-                    title: "",
+                    tabBarLabel: "",
                 }}
             />
             <Tabs.Screen
                 name="triage/index"
                 options={{
                     tabBarIcon: ({ color }) => <Triage fill={color} />,
-                    title: "",
+                    tabBarLabel: "",
                 }}
             />
             <Tabs.Screen
                 name="profile/index"
                 options={{
                     tabBarIcon: ({ color }) => <Profile fill={color} />,
-                    title: "",
+                    tabBarLabel: "",
                 }}
             />
         </Tabs>
